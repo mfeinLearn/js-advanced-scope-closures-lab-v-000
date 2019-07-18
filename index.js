fd900577b0ff1d9a2f5fb0f86ad7017a2a208015
github username <github email address>
2019-07-18 11:13:03 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function createDriver() {
+  let driverId = 0;
+  // return the class
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.blockRange = blockRange;
+      this.id = ++driverId;
+    }
+
+    produceDrivingRange(blockRange) {
+      return foo;
+      differenceFromRestToHome = restorantStreet - customerStreet;
+      // the difference between the restorant and the home
+      if (differenceFromRestToHome > this.blockRange) {
+        return `${differenceFromRestToHome} blocks out of range`
+          } else {
+        return `within range by ${differenceFromRestToHome}`
+      }// end of condition
+
+
+
+    }
+  };
+}
+
+const Driver = createDriver();
