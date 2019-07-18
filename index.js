27211c945518d78133dae06a5d8a46b90d26c1a9
github username <github email address>
2019-07-18 12:51:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
+function produceDrivingRange(blockRange) {
+  return function(restorantStreet, customerStreet) {
+    let restStreet = parseInt(restorantStreet);
+    let custStreet = parseInt(customerStreet);
+    let distanceToTravel = Math.abs(custStreet - restStreet);
+    let differenceFromRestToHome = blockRange - distanceToTravel;
+  // the difference between the restorant and the home
+    if (differenceFromRestToHome > 0) {
+      return `within range by ${differenceFromRestToHome}`
+        } else {
+      return `${Math.abs(differenceFromRestToHome)} blocks out of range`
+    }// end of condition
+  }
+}
+
+function produceTipCalculator(tip) {
+    return function(fair) {
+      return fair * tip
+    }
+}
+
 function createDriver() {
   let driverId = 0;
   // return the class
   return class {
     constructor(name) {
       this.name = name;
-      this.blockRange = blockRange;
       this.id = ++driverId;
-    }
-
-    produceDrivingRange(blockRange) {
-      return foo;
-      differenceFromRestToHome = restorantStreet - customerStreet;
-      // the difference between the restorant and the home
-      if (differenceFromRestToHome > this.blockRange) {
-        return `${differenceFromRestToHome} blocks out of range`
-          } else {
-        return `within range by ${differenceFromRestToHome}`
-      }// end of condition
-
-
-
     }
   };
 }
-
-const Driver = createDriver();
